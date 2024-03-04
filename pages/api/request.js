9e1df8e59597477e8c4eda95d1a5c8c80202f2f5
teampen <136991215+teampen@users.noreply.github.com>
2024-03-04 14:38:18 -0500

    added analytics

--- pages/api/request.js
+++ pages/api/request.js
@@ -1,19 +1,36 @@
 const cheerio = require('cheerio');
 const {SELECTOR_JS} = require("@/const/snippets");
 
-const PROXY_SERVER = process.env.PROXY_SERVER ? "proxy" : "local";
 const PROXY_API_KEY = process.env.PROXY_API_KEY;
 
 const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36";
 
+async function sendAnalytics(req){
+    const config = {
+        headers: req.headers,
+        body: JSON.stringify({
+            "name": "pageview",
+            "url": process.env.NEXT_PUBLIC_URL,
+            "domain": `${process.env.NEXT_PUBLIC_DOMAIN}`,
+            "props": {
+                "body": req.body
+            }
+        }),
+        "method": "post"
+    }
+    let resp = await fetch(`${process.env.ANALYTICS_ENDPOINT}/api/event`, config);
+    console.log(`Response status for analytics ${resp.status}`)
+}
 
 export default async function handler(req, res) {
+
     if (req.method === 'POST') {
+
         const body = req.body;
         console.log(`got post request with body ${JSON.stringify(body)}`)
 
         // This is where the proxy request is used
-        if (REQUEST_SERVER === "proxy"){
+        if (process.env.PROXY_ENDPOINT){
             const payload = body;
 
             payload["user-agent"] = USER_AGENT;
@@ -28,7 +45,11 @@ export default async function handler(req, res) {
                 "method": "post"
             }
 
-            const resp = await fetch(PROXY_ENDPOINT, config);
+            const resp = await fetch(`${process.env.PROXY_ENDPOINT}/v1/request`, config);
+
+            // analytics to prevent abuse
+            await sendAnalytics(req, res);
+
             const html = await resp.text();
             const $ = cheerio.load(html);
             // console.log(`appending selector js :${SELECTOR_JS}`)
